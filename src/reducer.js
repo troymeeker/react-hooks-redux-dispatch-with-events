6037849fca2bfded8a0fe7ab4e40854201c9d85d
troymeeker <troymeeker1@gmail.com>
2022-04-12 21:32:45 -0700

    reducer with state and increase & decrease

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,36 @@
-// add code snippets from README
+let state;
+
+function reducer(state = {count: 0}, action){
+    switch ( action.type ){
+        case "counter/increment":
+            return { count: state.count + 1 };
+        case "counter/decrement":
+            return { count: state.count -1};
+        default:
+            return state;
+    }
+
+}
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+function render(){
+    const container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+dispatch({type: '@@init'});
+
+const button = document.getElementById("button");
+
+button.addEventListener('click', ()=> {
+    dispatch({type: "counter/increment"});
+});
+
+const decrease = document.getElementById('decrease')
+decrease.addEventListener('click', ()=> {
+    dispatch({type: 'counter/decrement'})
+})
